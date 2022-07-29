8a0afb8035fd21771999c9efedf76aff716cd9a9
FelixLiUnsw <37093162+FelixLiUnsw@users.noreply.github.com>
2022-07-30 01:07:19 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from mycode import demo_simple
+
+if __name__ == '__main__':
+    #demo_simple.main(test = 'single') # if you want to test one single image  <--- run this first, and commemnt this line
+    #demo_simple.main(test = 'pre') # if you want to pred all images and save them in folder <--- if you run this line, please comment above line
+    demo_simple.main(test = 'acc') # if you want to get the accuarcy from the image folder. Make sure you have run above process <--- run this line, please comment above lines. 
+
